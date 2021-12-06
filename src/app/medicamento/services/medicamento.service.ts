3c4f479ddc912f3568e3b3047eaf6e34b9da48b0
MarcoJSB <marcojaziel690@gmail.com>
2021-12-06 00:11:09 -0600

    Pagos Stripe terminado v1

--- src/app/medicamento/services/medicamento.service.ts
+++ src/app/medicamento/services/medicamento.service.ts
@@ -2,19 +2,24 @@ import { Injectable } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 import { Observable } from 'rxjs';
 import { IMedicamento } from '../models/interfaces/medicamento.interface';
+import { IPagoStripeRequest, IPagoStripeResponse } from '../models/interfaces/pago.interface';
  
 @Injectable({
   providedIn: 'root'
 })
 export class MedicamentoService {
- 
+
   constructor(private http: HttpClient) { }
- 
+
   public getPaginatedMeds(take = 5, skip = 0): Observable<IMedicamento[]> {
     return this.http.get<IMedicamento[]>(`/medicamentos/paginate?take=${take}&skip=${skip}`);
   }
- 
+
   public getOne(id: string): Observable<IMedicamento> {
     return this.http.get<IMedicamento>(`/medicamentos/${id}`);
+  }
+
+  public createStripeOrder(body: IPagoStripeRequest): Observable<IPagoStripeResponse> {
+    return this.http.post<IPagoStripeResponse>(`/ordenes/stripe-orden`, body);
   }
 }
--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,4 +1,5 @@
 export const environment = {
   production: true,
-  apiEndpoint: 'https://medicamentos-api-10g1.herokuapp.com/api/v1'
+  apiEndpoint: 'https://medicamentos-api-10g1.herokuapp.com/api/v1',
+  stripePublishKey: 'pk_test_51JHKVXFBYqER2yeXpaK8aAasVa4hhdZ3Rzm8U2dtlE5TAppLgnDxihHX4eLuR5S3qSRyKVJ1wnBTVWddx8sDBYMi00K5NrUfTl'
 };
